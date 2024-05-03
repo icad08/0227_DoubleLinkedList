c5cdc368ae5a0b25c6d395693f4650b4d3f70ba7
icad08 <irsyadrm08@gmail.com>
2024-05-03 22:15:45 +0700

    tambahan dari else

--- CLL.cpp
+++ CLL.cpp
@@ -18,15 +18,26 @@ void addNode(){
     cout << "\nEnter the name of student: ";
     cin >> newNode -> name; // assign value to the data field of the new node
 
-    // insert the new code in the list
+    // insert the new node in the list
+    if  (START == NULL || newNode ->noMhs <= START->noMhs) {
+
     if (START == NULL && newNode -> noMhs == START  -> noMhs) {
        cout << "\033 [31mDuplicate roll number not allowed\033]0m" << endl;
        return;  
     }
     // if the list is empty, make the new node the START
     newNode -> next  = START; // step 3: make the new node point to the first node
-    if (START != NULL )
-        START= >prev  = newNode ;// step 4: make the first node point to the new node 
+    if (START != NULL ) {
+        START= ->prev  = newNode ;// step 4: make the first node point to the new node 
+    }
+    newNode->prev = newNode; // step 5: make the new node point to NULL
+    START = newNode; // step 6: make the new node the first node
+    }
+}
+else {
+    // insert the new node in the middle or at the end
+   node *current = START:
+   node *previous = NULL;
 }
 
 
