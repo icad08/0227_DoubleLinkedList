44f6ad917d0f9a4a47501dc117918b9ded6625f4
icad08 <irsyadrm08@gmail.com>
2024-05-03 22:31:23 +0700

    add else if and delete

--- CLL.cpp
+++ CLL.cpp
@@ -105,6 +105,17 @@ void deleteNode()
             START->prev = NULL;
         }
     }
+     else
+    {
+        previous->next = current->next;
+        if(current->next != NULL)
+        {
+            current->next->prev = previous;
+        }
+    }
+    delete current;
+    cout << "\x1b[32mRecord with roll number" << rollNo << "deleted\x1b[0m" << endl;
+}
 
 
 int main(){
